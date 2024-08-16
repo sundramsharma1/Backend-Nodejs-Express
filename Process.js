6579d4ff927481c76799e463e4f4d7b9e7449f66
sundramsharma1 <sundramsharma12244@gmail.com>
2024-08-16 15:28:29 +0530

    Adding process.js

---
+++ Process.js
@@ -0,0 +1,16 @@
+// To Run this file type on command prompt node Process.js
+
+// let n = 5;
+// for(let i = 0; i<=n; i++){
+//     console.log("Hello", i);
+// }
+
+
+
+//Type on command prompt node Process.js arguments, arguments are the values that are passed to the program when it is run.
+
+// let n = process.argv;
+// for(let i = 2; i < n.length; i++){
+//     console.log("Hiii", n[i]);
+// };
+
